43721a5b07ab32d0f87985ea29042f14447a1369
kispols <kispal.attila0106@gmail.com>
2022-01-28 09:35:24 +0100

    1.-2. feladat

--- 2022-01-28_Opening_Weekend/OpeningWeekend.cs
+++ 2022-01-28_Opening_Weekend/OpeningWeekend.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace _2022_01_28_Opening_Weekend
 {
-    class Program
+    class OpeningWeekend
     {
         static void Main(string[] args)
         {
+            List<Film> filmek = new List<Film>();
+            foreach (var sor in File.ReadAllLines("nyitohetvege.txt").Skip(1))
+            {
+                filmek.Add(new Film(sor));
+            }
+
             Console.ReadKey();
         }
     }
