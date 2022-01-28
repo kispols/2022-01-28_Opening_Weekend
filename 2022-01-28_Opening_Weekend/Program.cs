709a8e82e338ac207caf2c851a2bf2049d22b570
kispols <kispal.attila0106@gmail.com>
2022-01-28 09:11:48 +0100

    Add project files.

---
+++ 2022-01-28_Opening_Weekend/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022_01_28_Opening_Weekend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
